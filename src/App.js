016dbdf7457c8b6a5c34d6111348f715d4487ff8
MarkCodez <markeskander745@gmail.com>
2023-12-14 15:46:41 -0600

    made input field for user to add quick deposit and function to add deposit to user's total. removed a lot of unecessary react stuff and CSS

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-const userData = {
-  totalMoney: 1000.0
-};
 
 function App() {
+  // state hooks to hold store a manage variable data
+  const [addAmount, setAmount] = useState("0.00")
+  const [userData, setUserData] = useState({ totalMoney: 1000.0 });
+
+  // function to add amount that user input to total money amount
+  const addDeposit = () => {
+    const amountToAdd = parseFloat(addAmount); // string to float conversion
+    setUserData({ userData, totalMoney: userData.totalMoney + amountToAdd });
+  }
+
+  // changes input field to user's input
+  const change = event => {
+    setAmount(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <h3>Helloooo asdf hey</h3>
-        </a>
-
-
-        Enter an amount to deposit to your account:
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <div className='addingSection'>
+        Quick account deposit
         <br />
         <label>
-          Dollar amount: <input name="addAmount" />
+          Dollar amount $:
+          <input
+            onChange={change}
+            value={addAmount}
+            type='number'
+            defaultValue={"0.00"} />
         </label>
 
-      </header>
+        <div>
+          <button onClick={addDeposit}>
+            Add
+          </button>
+        </div>
+
+        <div>
+          Total money: {userData.totalMoney.toFixed(2)}
+        </div>
+
+      </div>
     </div>
   );
 }
